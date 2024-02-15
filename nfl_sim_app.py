c88b7dafe3abe68bcd757d14b6fa8a59a1f28b88
Christopher Davis <christopherjdavis@gmail.com>
2024-02-14 20:03:58 -0800

    Update nfl_sim_app.py

--- nfl_sim_app.py
+++ nfl_sim_app.py
@@ -374,9 +374,9 @@ if 'pc' in st.session_state:
     st.altair_chart(st.session_state["stage_charts"])
     #Week 18, no need for best regular season record
     #st.altair_chart(st.session_state["best_chart"])
-    df_temp = pd.read_csv("data/markets.csv")
-    st.write(f'''The following were last updated on {df_temp.loc[0, 'date']}.  Needless to say, do not take the kelly staking sizes literally!!  (Along with errors and imprecisions in our app, also keep in mind how long the stake will be sitting unused.)''')
-    st.dataframe(st.session_state['raw_data'])
+    # df_temp = pd.read_csv("data/markets.csv")
+    # st.write(f'''The following were last updated on {df_temp.loc[0, 'date']}.  Needless to say, do not take the kelly staking sizes literally!!  (Along with errors and imprecisions in our app, also keep in mind how long the stake will be sitting unused.)''')
+    # st.dataframe(st.session_state['raw_data'])
     # st.altair_chart(st.session_state['lc'])
     # st.altair_chart(st.session_state["streak_charts"])
 else:
