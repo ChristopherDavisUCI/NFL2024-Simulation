2264ffe19cf5f177adbf56f43415d40fba8c795e
Christopher Davis <82750240+ChristopherDavisUCI@users.noreply.github.com>
2024-12-23 15:55:42 -0600

    Update make_charts.py

--- make_charts.py
+++ make_charts.py
@@ -105,7 +105,7 @@ def make_conf_champ_charts(matchup_dct):
             alt.X("team1").title(None),
             alt.Y("team2").title(None),
         ).properties(
-            width=150
+            width=200
         )
         text = base.mark_text().encode(text="prob")
         rect = base.mark_rect().encode(
